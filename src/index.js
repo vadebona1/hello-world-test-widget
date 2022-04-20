6276360c6ca179a79a1b57e815c8dd4dce45c9c3
Vicky Adebona <victoria@medlmobile.com>
2022-04-19 17:30:19 -0700

    importing existing hello world react project

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Select all instances of the widget
+const WidgetDivs = document.querySelectorAll('.hello-world-widget');
+
+WidgetDivs.forEach(div => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App domElement={div} />
+    </React.StrictMode>,
+    div
+  );
+});
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
