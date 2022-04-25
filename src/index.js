05e86d7d79400ee6a0c602c1ced0851d38a140cd
Vicky Adebona <victoria@medlmobile.com>
2022-04-25 16:10:55 -0700

    trying mount/unmount functions again

--- src/index.js
+++ src/index.js
@@ -5,16 +5,27 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // Select all instances of the widget
-const WidgetDivs = document.querySelectorAll('#hello-world-widget');
+// const WidgetDivs = document.querySelectorAll('#hello-world-widget');
 
-WidgetDivs.forEach(div => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App domElement={div} />
-    </React.StrictMode>,
-    div
-  );
-});
+// WidgetDivs.forEach(div => {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <App domElement={div} />
+//     </React.StrictMode>,
+//     div
+//   );
+// });
+
+window.HelloWorldWidget = {
+  mount: () => {
+    const el = document.getElementById('hello-world-widget');
+    ReactDOM.render(<App />, el);
+  },
+  unmount: () => {
+    const el = document.getElementById('hello-world-widget');
+    ReactDOM.unmountComponentAtNode(el);
+  }
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
